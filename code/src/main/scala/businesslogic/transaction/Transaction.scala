2c5d83743efc5935dcd304a320caba522f929484
claudiusbr <claudiusbr@users.noreply.github.com>
2018-03-13 07:58:54 +0000

    clean up: fixing what the IDE did

---
+++ code/src/main/scala/businesslogic/transaction/Transaction.scala
@@ -0,0 +1,23 @@
+package businesslogic
+package transaction
+
+/**
+  * This is the control class which restricts access to the creation of entries
+  * on categories.
+  */
+class Transaction {
+
+  /**
+    * This method validates and executes a transaction executed by a user
+    */
+  //def execute(operands: List[Tuple2[Category,List[Entry]]]): List[Category] = { }
+
+  /**
+    * this method takes a list of entries and adds them up to make sure they
+    * equal zero, thus preserving double entry
+    * @param entries a list of entries
+    * @return true if the sum of the amounts of all entries equals zero
+    */
+  def sumEqualsZero(entries: List[Entry]): Boolean =
+    entries.foldLeft(0.0d)((s: Double, e: Entry) => s + e.amount.total) == 0.0d
+}
