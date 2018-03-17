2efe5759e4743571a840b32e610246ba371cf2e6
claudiusbr <claudiusbr@users.noreply.github.com>
2018-03-17 21:11:01 +0000

    more tests for EntryParser

--- code/src/test/scala/personalfinance/businesslogic/transaction/EntryParserTester.scala
+++ code/src/test/scala/personalfinance/businesslogic/transaction/EntryParserTester.scala
@@ -14,10 +14,26 @@ class EntryParserTester extends BehaviourTester with Mocker {
   when(mockDateRegistryFactory.getDateRegistry("2018-01-03"))
     .thenReturn(mockDateRegistry)
 
+  private val expectedResult = List(
+    Entry(200.00,mockDateRegistry,"blabla"),
+    Entry(-10.00,mockDateRegistry,"bleble"))
+
   private val testContents = List(
     "date,description,amount",
     "2018-01-03,blabla,200.00",
     "2018-01-03,bleble,-10.00"
+  )
+
+  private val diffOrder = List(
+    "description,date,amount",
+    "blabla,2018-01-03,200.00",
+    "bleble,2018-01-03,-10.00"
+  )
+
+  private val diffHeaders = List(
+    "transaction description,transaction date,credit/debit",
+    "blabla,2018-01-03,200.00",
+    "bleble,2018-01-03,-10.00"
   )
 
   private val mockPL = mock[PropertiesLoader]
@@ -28,9 +44,20 @@ class EntryParserTester extends BehaviourTester with Mocker {
   private val ep = new EntryParser(mockDateRegistryFactory)
 
   "an EntryParser" should "parse file contents into Entry instances" in{
-    ep.parseCSVLines(testContents,mockPL) should be (List(
-      Entry(200.00,mockDateRegistry,"blabla"),
-      Entry(-10.00,mockDateRegistry,"bleble")))
+    ep.parseCSVLines(testContents,mockPL) should be (expectedResult)
   }
 
+  it should "parse the files even if the columns are not in order" in {
+    ep.parseCSVLines(diffOrder,mockPL) should be (expectedResult)
+  }
+
+  it should "parse the files even with columns have different headers" in {
+
+    when(mockPL.getProperty("date")).thenReturn("transaction date")
+    when(mockPL.getProperty("description"))
+      .thenReturn("transaction description")
+    when(mockPL.getProperty("amount")).thenReturn("credit/debit")
+
+    ep.parseCSVLines(diffHeaders,mockPL) should be (expectedResult)
+  }
 }
