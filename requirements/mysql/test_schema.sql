2457d264cd4da678343664e7aadf194c2edfef7d
claudiusbr <claudiusbr@users.noreply.github.com>
2018-04-21 16:22:44 +0100

    new test schema

--- requirements/mysql/test_schema.sql
+++ requirements/mysql/test_schema.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 5.7.21, for Linux (x86_64)
 --
--- Host: localhost    Database: personal_finance_system
+-- Host: localhost    Database: test_personal_finance
 -- ------------------------------------------------------
 -- Server version	5.7.21-0ubuntu0.16.04.1
 
@@ -27,7 +27,7 @@ CREATE TABLE `category` (
   `name` varchar(100) NOT NULL,
   PRIMARY KEY (`idcategory`),
   UNIQUE KEY `name_UNIQUE` (`name`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -36,7 +36,7 @@ CREATE TABLE `category` (
 
 LOCK TABLES `category` WRITE;
 /*!40000 ALTER TABLE `category` DISABLE KEYS */;
-INSERT INTO `category` VALUES (1,'Bank');
+INSERT INTO `category` VALUES (1,'Bank'),(3,'groceries'),(2,'hardware');
 /*!40000 ALTER TABLE `category` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -59,7 +59,7 @@ CREATE TABLE `entry` (
   KEY `fk_entry_2_idx` (`description_id`),
   CONSTRAINT `fk_entry_1` FOREIGN KEY (`category_id`) REFERENCES `category` (`idcategory`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `fk_entry_2` FOREIGN KEY (`description_id`) REFERENCES `entry_description` (`id_entry_description`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -68,6 +68,7 @@ CREATE TABLE `entry` (
 
 LOCK TABLES `entry` WRITE;
 /*!40000 ALTER TABLE `entry` DISABLE KEYS */;
+INSERT INTO `entry` VALUES (5,'2017-04-21 00:00:00','2017-04-21 00:00:00',-500,1,1),(6,'2017-04-21 00:00:00','2017-04-21 00:00:00',500,3,1),(7,'2018-04-21 00:00:00','2018-04-21 00:00:00',-600,1,2),(8,'2018-04-21 00:00:00','2018-04-21 00:00:00',600,3,2),(9,'2018-03-10 00:00:00','2018-03-10 00:00:00',-30,1,3),(10,'2018-03-10 00:00:00','2018-03-10 00:00:00',30,1,3),(11,'2018-03-17 00:00:00','2018-03-17 00:00:00',-40,1,4),(12,'2018-03-17 00:00:00','2018-03-17 00:00:00',40,1,4);
 /*!40000 ALTER TABLE `entry` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -82,7 +83,7 @@ CREATE TABLE `entry_description` (
   `id_entry_description` int(11) NOT NULL AUTO_INCREMENT,
   `value` varchar(100) NOT NULL,
   PRIMARY KEY (`id_entry_description`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -91,6 +92,7 @@ CREATE TABLE `entry_description` (
 
 LOCK TABLES `entry_description` WRITE;
 /*!40000 ALTER TABLE `entry_description` DISABLE KEYS */;
+INSERT INTO `entry_description` VALUES (1,'a new laptop from PCWorld'),(2,'a laptop from some store I found'),(3,'weekly shopping from local grocer'),(4,'monthly shopping from supermarket');
 /*!40000 ALTER TABLE `entry_description` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -109,7 +111,7 @@ CREATE TABLE `pattern` (
   UNIQUE KEY `value_UNIQUE` (`value`),
   KEY `fk_pattern_1_idx` (`category_id`),
   CONSTRAINT `fk_pattern_1` FOREIGN KEY (`category_id`) REFERENCES `category` (`idcategory`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -118,6 +120,7 @@ CREATE TABLE `pattern` (
 
 LOCK TABLES `pattern` WRITE;
 /*!40000 ALTER TABLE `pattern` DISABLE KEYS */;
+INSERT INTO `pattern` VALUES (1,'new laptop',2),(2,'laptop from some store',2),(3,'weekly shopping',3),(4,'monthly shopping',3);
 /*!40000 ALTER TABLE `pattern` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -130,4 +133,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-03-18 16:28:12
+-- Dump completed on 2018-04-21 15:35:58
